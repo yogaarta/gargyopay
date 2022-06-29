ae91f8afa0485cf9f2e624f3687f9cda72fb2950
Yoga Arta G <grahanantyo@gmail.com>
2022-06-30 01:53:27 +0700

    landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Landing.module.css'
+import { Telephone, Lock, Download, ArrowLeft, ArrowRight } from 'react-bootstrap-icons'
 
+import Sponsor from '../assets/img/sponsor.png'
 import Phone from '../assets/img/phone.png'
+import Alex from '../assets/img/alex.png'
 
 export default function Landing() {
   return (
@@ -12,7 +15,7 @@ export default function Landing() {
           <div className={styles.logo}>GargyoPay</div>
           <div className={styles.authContainer}>
             <div className={styles.loginButton}>Login</div>
-            <div className={styles.signUpButton}>Register</div>
+            <div className={styles.signUpButton}>Sign Up</div>
           </div>
         </div>
         <div className={styles.banner2}>
@@ -21,6 +24,78 @@ export default function Landing() {
           <div className={styles.button}>Try It Free</div>
         </div>
       </div>
+      <section className={styles.section2}>
+        <div className={styles.title}><span>Why</span> Choose GargyoPay?</div>
+        <div className={styles.info}>We have some great features from the application and it's totally free to use by all users around the world.</div>
+        <div className={styles.infoContainer}>
+          <div className={styles.side}>
+            <div className={styles.imgContainer}><Telephone className={styles.icon} /></div>
+            <div className={styles.itemTitle}>24/7 Support</div>
+            <div className={styles.itemInfo}>We have 24/7 contact support so you can contact us whenever you want and we will respond it.</div>
+          </div>
+          <div className={styles.center}>
+            <div className={styles.imgContainer}><Lock className={styles.icon} /></div>
+            <div className={styles.itemTitle}>Data Privacy</div>
+            <div className={styles.itemInfo}>We make sure your data is safe in our database and we will encrypt any data you submitted to us.</div>
+          </div>
+          <div className={styles.side}>
+            <div className={styles.imgContainer}><Download className={styles.icon} /></div>
+            <div className={styles.itemTitle}>Easy Download</div>
+            <div className={styles.itemInfo}>Zwallet is 100% totally free to use it's now available on Google Play Store and App Store.</div>
+          </div>
+        </div>
+      </section>
+      <section className={styles.section3}>
+        <Image src={Sponsor} />
+      </section>
+      <section className={styles.section4}>
+        <div className={styles.money}>Rp. 390.736.500</div>
+        <div className={styles.title}><span>Money</span> has Been Transfered.</div>
+        <div className={styles.info}>That amount of money has been transfered from all users.<br /> We still counting and going strong!</div>
+      </section>
+      <section className={styles.section5}>
+        <aside><Image src={Phone} /></aside>
+        <main>
+          <div className={styles.title}>All The <span>Great</span><br />GargyoPay Features.</div>
+          <div className={styles.itemContainer}>
+            <div className={styles.number}><span>1.</span> Small Fee</div>
+            <div className={styles.info}>We only charge 5% of every success transaction done in GargyoPay app.</div>
+          </div>
+          <div className={styles.itemContainer}>
+            <div className={styles.number}><span>2.</span> Data Secured</div>
+            <div className={styles.info}>All your data is secured properly in our system and it's encrypted.</div>
+          </div>
+          <div className={styles.itemContainer}>
+            <div className={styles.number}><span>3.</span> User Friendly</div>
+            <div className={styles.info}>GargyoPay come up with modern and sleek design and not complicated.</div>
+          </div>
+        </main>
+      </section>
+      <section className={styles.section6}>
+        <div className={styles.title}>What Users are <span>Saying</span>.</div>
+        <div className={styles.info}>We have some great features from the application and it's totally free <br /> to use by all users around the world.</div>
+        <div className={styles.reviewContainer}>
+          <div className={styles.arrow}><ArrowLeft /></div>
+          <div className={styles.reviewBox}>
+            <div className={styles.imgContainer}><Image src={Alex} className={styles.alex} /></div>
+            <div className={styles.name}>Alex Hansinburg</div>
+            <div className={styles.job}>Designer</div>
+            <div className={styles.review}>“This is the most outstanding app that I've ever try in my live, this app is such an amazing masterpiece and it’s suitable for you who is bussy with their bussiness and must transfer money to another person aut there. Just try this app and see the power!”</div>
+          </div>
+          <div className={styles.arrow}><ArrowRight /></div>
+        </div>
+      </section>
+      <footer className={styles.footer}>
+        <div className={styles.title}>GargyoPay</div>
+        <div className={styles.info}>Simplify financial needs and saving much time in banking needs with one single app.</div>
+        <div className={styles.bottom}>
+          <div className={styles.copyright}>2022 GargyoPay. All right reserved.</div>
+          <div className={styles.right}>
+            <div className={styles.phone}>+62 8224 2161 766</div>
+            <div className={styles.email}>contact@gargyopay.com</div>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
